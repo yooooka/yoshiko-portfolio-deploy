889ad2d2d190dfd4cb0de8af18c6c75cdb2328b6
yooooka <yooooka@gmail.com>
2014-03-03 11:31:04 +0900

    Update top.js
    took off mouse out effect

--- jquery/top.js
+++ jquery/top.js
@@ -1,4 +1,3 @@
-
 // Top thumbnails
 $(function() {
 	// OPACITY OF BUTTON SET TO 0%
@@ -7,10 +6,10 @@ $("#topthumb_web li img, #topthumb_go li img").css("opacity","0");
 $("#topthumb_web li img, #topthumb_go li img").hover(function () {
 	// SET OPACITY TO 100%
 $(this).stop().animate({opacity: 1.0}, "normal");
-},
-	// ON MOUSE OUT
-function () {
-	// SET OPACITY BACK TO 00%
-$(this).stop().animate({opacity: 0}, "normal");
+//},
+//	// ON MOUSE OUT
+//function () {
+//	// SET OPACITY BACK TO 00%
+//$(this).stop().animate({opacity: 0}, "normal");
 });
 });
